6f38f5fadd857593d0ee6b24b9aa6a56d825367e
ambiti0n <fabulousfresh010@gmail.com>
2022-04-21 02:48:41 +0100

    successfully uploaded file to ifps using moralise savefile

--- components/UploadForm.js
+++ components/UploadForm.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import { useState } from "react";
+import { useMoralisFile } from "react-moralis";
 
 export default function UploadForm({ logout }) {
   const [input, setInput] = useState({
@@ -15,6 +16,8 @@ export default function UploadForm({ logout }) {
       [e.target.name]: e.target.value,
     }));
   };
+
+  const { saveFile } = useMoralisFile();
 
   console.log(inputFile);
   return (
@@ -33,7 +36,19 @@ export default function UploadForm({ logout }) {
       </div>
       <div className="flex items-center justify-center overflow-y-hidden">
         <div className="w-2/3 max-w-screen mt-6">
-          <form>
+          <form
+            onSubmit={async (e) => {
+              e.preventDefault();
+              if (inputFile !== null) {
+                await saveFile(input.nftName, inputFile, {
+                  saveIPFS: true,
+                  onSuccess: (file) => {
+                    console.log(file);
+                  },
+                });
+              }
+            }}
+          >
             <div className="shadow sm:rounded-md sm:overflow-hidden">
               <div className="px-4 py-5 bg-white space-y-6 sm:p-6">
                 <div className="grid grid-cols-3 gap-6">
@@ -87,20 +102,24 @@ export default function UploadForm({ logout }) {
                   </label>
                   <div className="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
                     <div className="space-y-1 text-center">
-                      <svg
-                        className="mx-auto h-12 w-12 text-gray-400"
-                        stroke="currentColor"
-                        fill="none"
-                        viewBox="0 0 48 48"
-                        aria-hidden="true"
-                      >
-                        <path
-                          d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
-                          strokeWidth={2}
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                        />
-                      </svg>
+                      {inputFile !== null ? (
+                        <p>{inputFile.name}</p>
+                      ) : (
+                        <svg
+                          className="mx-auto h-12 w-12 text-gray-400"
+                          stroke="currentColor"
+                          fill="none"
+                          viewBox="0 0 48 48"
+                          aria-hidden="true"
+                        >
+                          <path
+                            d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
+                            strokeWidth={2}
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                          />
+                        </svg>
+                      )}
                       <div className="flex text-sm text-gray-600">
                         <label
                           htmlFor="file-upload"
