6391bbf8e03a4645eb33519270694199081335cc
ambiti0n <fabulousfresh010@gmail.com>
2022-04-21 02:13:27 +0100

    authenticate  and logout methods from usemoralis methods implemented

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,10 @@ import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <MoralisProvider>
+    <MoralisProvider
+      appId={process.env.NEXT_PUBLIC_APP_ID}
+      serverUrl={process.env.NEXT_PUBLIC_SERVER_URL}
+    >
       <Component {...pageProps} />
     </MoralisProvider>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,29 @@
 import AuthPage from "../components/AuthPage";
+import { useMoralis } from "react-moralis";
 import UploadForm from "../components/UploadForm";
 
 export default function Home() {
+  const { isAuthenticated, authenticate, logout } = useMoralis();
+
+  const metamaskAuth = async () => {
+    try {
+      await authenticate({
+        signInMessage: "Auth to start lazy minting your Nfts",
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  if (!isAuthenticated) {
+    return (
+      <div>
+        <AuthPage metamaskAuth={metamaskAuth} />
+      </div>
+    );
+  }
   return (
     <div>
-      {/* <AuthPage /> */}
-      <UploadForm />
+      <UploadForm logout={logout} />
     </div>
-  )
+  );
 }
